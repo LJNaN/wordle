ea7d6aec18d3bc9db42428d2024140d38ddc557f
jiangnan <49558206@qq.com>
2025-01-06 17:54:10 +0800

    ./

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), WindiCSS()],
+  base: './',
   assetsInclude: ['**/*.csv'],
   resolve: {
     alias: {
